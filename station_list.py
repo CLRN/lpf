3b95b1f3da0f7e7d7c5d1794322163e2bbf348bc
clrn <clrnmail@gmail.com>
2018-05-07 12:22:41 +0100

    initial code

---
+++ station_list.py
@@ -0,0 +1,52 @@
+from requests_html import HTMLSession
+from mongoengine import Document, connect, StringField
+
+connect("lpf")
+
+
+class Station(Document):
+    name = StringField(required=True)
+    location = StringField()
+    code = StringField(required=True)
+
+    def __repr__(self):
+        return "[{}] {}".format(self.code, self.name)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __unicode__(self):
+        return self.__repr__()
+
+
+class StationList:
+    def __init__(self):
+        self.session = HTMLSession()
+        self.url = 'https://en.wikipedia.org/wiki/List_of_London_railway_stations'
+        self.stations = list()
+
+        if not len(Station.objects):
+            self.save()
+        self.load()
+
+    def load(self):
+        self.stations = [s for s in Station.objects]
+
+    def save(self):
+        response = self.session.get(self.url)
+        for row in response.html.find('tr'):
+            if len(row.links) == 1:
+                continue
+
+            data = [r.strip() for r in row.text.split('\n')]
+            if len(data) < 8:
+                continue
+
+            Station(name=data[0], location=data[7], code=data[3]).save()
+
+
+if __name__ == '__main__':
+    l = StationList()
+    print(l.stations)
+
+
