bd8b06ae1468a576a1a9c5a2d82b57880e3fe680
clrn <clrnmail@gmail.com>
2024-08-06 19:50:20 +0000

    fetch station list

---
+++ station_fetcher.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+
+
+def load() -> pd.DataFrame:
+    content = requests.get("https://www.allthestations.co.uk/map/allthestations-js-ie.php").content.decode()
+    content = content[content.find("oaMarkers[1] =") :]
+    pos = 0
+
+    items = []
+    while pos != -1:
+        pos = content.find("] =", pos)
+        begin = content.find("({", pos)
+        end = content.find("})", begin)
+        pos = content.find("] =", end)
+
+        body = content[begin:end]
+
+        lines = body.split("\n")
+        where = [line for line in lines if line.find("position") != -1][0]
+        title = [line for line in lines if line.find("title") != -1][0]
+
+        parts = where.split(" ")
+        parts = ["".join([c for c in s if c == "." or c.isdigit()]) for s in parts]
+        lat, lng = list(map(float, filter(str, parts)))
+
+        station = title.split("'")[1]
+        beg = station.find("(")
+        end = station.find(")")
+        code = station[beg + 1 : end]
+        station = station[: beg - 1]
+
+        items.append({"code": code, "name": station, "lat": lat, "lng": lng})
+
+    return pd.DataFrame(items)
+
+
+def main():
+    df = load()
+    df.to_csv("data/stations.csv")
+
+
+if __name__ == "__main__":
+    main()
